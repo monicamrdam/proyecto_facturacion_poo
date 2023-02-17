ee26ef4e3083939ddbd20c9dc1bd096cd10e8888
monicamrdam <monicamrdam@gmail.com>
2023-02-17 18:42:30 +0100

    update metodo mostrar

--- videojuegos_facturacion_poo_nivel2/TiendaVideojuegos.java
+++ videojuegos_facturacion_poo_nivel2/TiendaVideojuegos.java
@@ -11,7 +11,7 @@ public class TiendaVideojuegos
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         String opcion="";
-       
+
         do{
             System.out.println("\nIntroduzca la opción que desea realizar:\n"
                 +"1. Insertar Productos \n"
@@ -20,32 +20,33 @@ public class TiendaVideojuegos
                 +"4. Mostrar caja\n"
                 +"5. Emitir factura\n"
                 +"0. Salir\n"
-                );            
+            );            
             opcion=sc.nextLine();
             switch(opcion){
-            case "1":
-                System.out.println("Has seleccionado insertar productos");
-                ControlProductos.solicitarDatosParaRegistrar();
-                break;
-            case "2":
-                System.out.println("Has seleccionado mostrar productos");
-                break;
-            case "3":
-                System.out.println("Has seleccionado comprar productos");
-                break;
-            case "4":
-                System.out.println("Has seleccionado mostrar caja");
-                break;
-            case "5":
-                System.out.println("Has seleccionado emitir factura");
-                break;
-            case "0":
-                break;
-            default:
-                System.out.println("Has seleccionado una opción incorrecta");
+                case "1":
+                    System.out.println("Has seleccionado insertar productos");
+                    ControlProductos.solicitarDatosParaRegistrar();
+                    break;
+                case "2":
+                    System.out.println("Has seleccionado mostrar productos");
+                    ControlProductos.mostrarProductos();
+                    break;
+                case "3":
+                    System.out.println("Has seleccionado comprar productos");
+                    break;
+                case "4":
+                    System.out.println("Has seleccionado mostrar caja");
+                    break;
+                case "5":
+                    System.out.println("Has seleccionado emitir factura");
+                    break;
+                case "0":
+                    break;
+                default:
+                    System.out.println("Has seleccionado una opción incorrecta");
             }
         }while(!opcion.equals("0"));
         System.out.println("Programa finalizado");
-        
+
     }
 }
