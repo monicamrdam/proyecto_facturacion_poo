11c5264cd50fe629bcdd133ce59553f3c88bdd27
monicamrdam <monicamrdam@gmail.com>
2023-02-17 10:04:30 +0100

    add clase DetalleComprobante

---
+++ DetalleComprobante.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class DetalleComprobante here.
+ * Clase detalle comprobante
+ * @author (monicamrdam) 
+ * @version (17/02/2023)
+ */
+public class DetalleComprobante
+{
+    git 
+}
