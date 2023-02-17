75c60fff4f25ef042e71efa5f59d90e73bb8b8df
monicamrdam <monicamrdam@gmail.com>
2023-02-17 18:44:49 +0100

    update metodos insertar cine y videojuegos

--- videojuegos_facturacion_poo_nivel2/ControlProductos.java
+++ videojuegos_facturacion_poo_nivel2/ControlProductos.java
@@ -12,32 +12,35 @@ import java.io.IOException;
  */
 public class ControlProductos
 {
+    private static final String NOMBRE_ARCHIVO_CINE = "cine.txt";
     private static final String NOMBRE_ARCHIVO_MUSICA = "musica.txt";
+    private static final String NOMBRE_ARCHIVO_VIDEOJUEGOS = "videojuegos.txt";
     private static final String SEPARADOR_CAMPO = "; ";
     private static final String SEPARADOR_REGISTRO = "\n";
     private static CaracteristicasProductos cproducto;
     private static Scanner sc = new Scanner(System.in);
-    
-    
+
     private Producto productos[] = null;
-  private double caja;
-  
-  //Constructors
-  public ControlProductos() { }
-  public ControlProductos(Producto[] productos) {
-    this.productos = productos;
-  }
-  
-  public static void solicitarDatosParaRegistrar(){
-      String opcion="";
-      System.out.println("\nQue tipo de producto es:\n"
-      +"1. Cine \n"
-      +"2. Musica \n"
-      +"3. Videojuegos \n");            
-      opcion=sc.nextLine();
-      switch(opcion){
+    private double caja;
+
+    //Constructors
+    public ControlProductos() { }
+
+    public ControlProductos(Producto[] productos) {
+        this.productos = productos;
+    }
+
+    public static void solicitarDatosParaRegistrar(){
+        String opcion="";
+        System.out.println("\nQue tipo de producto es:\n"
+            +"1. Cine \n"
+            +"2. Musica \n"
+            +"3. Videojuegos \n");            
+        opcion=sc.nextLine();
+        switch(opcion){
             case "1":
                 System.out.println("Has seleccionado insertar producto: cine");
+                ControlProductos.solicitarDatosParaRegistrarCine();
                 break;
             case "2":
                 System.out.println("Has seleccionado insertar producto: musica");
@@ -45,113 +48,165 @@ public class ControlProductos
                 break;
             case "3":
                 System.out.println("Has seleccionado insertar producto: videojuegos");
+                ControlProductos.solicitarDatosParaRegistrarVideojuegos();
                 break;
             default:
                 System.out.println("Has seleccionado una opción incorrecta");
-            }
-        
-    }
-  
-    private static boolean isDouble(String cadena){
-    try {
-        Double.parseDouble(cadena);
-        return true;
-    } catch (NumberFormatException nfe){
-        return false;
-    }
-}
-    private static boolean isInt(String cadena){
-    try {
-        Integer.parseInt(cadena);
-        return true;
-    } catch (NumberFormatException nfe){
-        return false;
-    }
-}
-  
-  public static CaracteristicasProductos solicitarDatosParaRegistrarProductos() {
-          System.out.println("Ingrese el nombre: ");
-          String nombre= sc.nextLine();
-          String precioUnit;
-          do{
-              System.out.println("Ingrese precio de la unidad: ");
-              precioUnit = sc.nextLine();
-          }while(!ControlProductos.isDouble(precioUnit));
-          String cantStock;
-          do{
-              System.out.println("Ingrese la cantidad de productos: ");
-              cantStock = sc.nextLine();
-          }while(!ControlProductos.isInt(cantStock));
-          String disponible="";
-          String respuesta;
-          do{
-
-              System.out.println("Ingrese si esta disponible: (si/no) ");
-              respuesta= sc.nextLine();
-              if (respuesta.toUpperCase().equals("SI")){
-                 disponible="true";
-              }else if(respuesta.toUpperCase().equals("N0")){
-                  disponible="false";
-              }else{
-                  System.out.print("Opcion incorrecta");
-              }
-    
-          }while(!(disponible.equals("true") || disponible.equals("false")));
-          cproducto = new CaracteristicasProductos(nombre, Double.parseDouble(precioUnit), Integer.parseInt(cantStock), Boolean.parseBoolean(disponible));
-          return cproducto;
-          
         }
-    public static String solicitarDatosCantante(){
-        System.out.println("Ingrese el cantante: ");
-        String cantante = sc.nextLine();
-        return cantante;
-    }
-    public static void solicitarDatosParaRegistrarMusica(){      
-        ControlProductos.registrar(new Musica(ControlProductos.solicitarDatosParaRegistrarProductos(),ControlProductos.solicitarDatosCantante()));
-        System.out.println("Registrado exitosamente");
+
     }
 
-  public static void registrar(Musica musica) {
+    private static boolean isDouble(String cadena){
         try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO_MUSICA, true));
-            bufferedWriter.write(musica.getNombre() + SEPARADOR_CAMPO + musica.getPrecioUnit() + SEPARADOR_CAMPO
-                    + musica.getCantStock() + SEPARADOR_CAMPO + musica.isDisponible() + SEPARADOR_CAMPO+ musica.getCantante() + SEPARADOR_REGISTRO );
+            Double.parseDouble(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+    }
+
+    private static boolean isInt(String cadena){
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+    }
+
+    public static CaracteristicasProductos solicitarDatosParaRegistrarProductos() {
+        System.out.println("Ingrese el nombre: ");
+        String nombre= sc.nextLine();
+        String precioUnit;
+        do{
+            System.out.println("Ingrese precio de la unidad: ");
+            precioUnit = sc.nextLine();
+        }while(!ControlProductos.isDouble(precioUnit));
+        String cantStock;
+        do{
+            System.out.println("Ingrese la cantidad de productos: ");
+            cantStock = sc.nextLine();
+        }while(!ControlProductos.isInt(cantStock));
+        String disponible="";
+        String respuesta;
+        do{
+
+            System.out.println("Ingrese si esta disponible: (si/no) ");
+            respuesta= sc.nextLine();
+            if (respuesta.toUpperCase().equals("SI")){
+                disponible="true";
+            }else if(respuesta.toUpperCase().equals("N0")){
+                disponible="false";
+            }else{
+                System.out.print("Opcion incorrecta");
+            }
+
+        }while(!(disponible.equals("true") || disponible.equals("false")));
+        cproducto = new CaracteristicasProductos(nombre, Double.parseDouble(precioUnit), Integer.parseInt(cantStock), Boolean.parseBoolean(disponible));
+        return cproducto;
+
+    }
+
+    public static String solicitarDatosDirector(){
+        System.out.println("Ingrese el director de cine: ");
+        String director = sc.nextLine();
+        return director;
+    }
+
+    public static void registrar(Cine cine) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO_CINE, true));
+            bufferedWriter.write(cine.getNombre() + SEPARADOR_CAMPO + cine.getPrecioUnit() + SEPARADOR_CAMPO
+                + cine.getCantStock() + SEPARADOR_CAMPO + cine.isDisponible() + SEPARADOR_CAMPO+ cine.getDirector() + SEPARADOR_REGISTRO );
             bufferedWriter.close();
         } catch (IOException e) {
             System.out.println("Error escribiendo en archivo: " + e.getMessage());
         }
     }
-    
-    
-    
-    
-    
-    
 
-   public void mostrarProductos(Producto[] productos) { 
-      for (int i = 0; i < productos.length; i++) {
-        System.out.print(productos[i]+"n-------n");
-      }
-  }
-   public void mostrarNombreProductos(Producto[] productos) { 
-      for (int i = 0; i < productos.length; i++) {
-        System.out.println(i+1 +" "+productos[i].getNombre()+"n");
-      }
-      System.out.println("n------------n");
-  } 
-  public double comprarProducto(Producto[] productos, int num, int cantidadUnidades) {
-    if (productos[num-1].isDisponible()) {
-      if (productos[num-1].getCantStock() >= cantidadUnidades){
-        System.out.println("La compra se ha realizado con éxito!!n");
-        productos[num -1].setCantStock(productos[num -1].getCantStock()-cantidadUnidades);
-        return caja+=cantidadUnidades*productos[num-1].getPrecioUnit();
-      } else {System.out.println("No hay cantidad suficiente de producto");}
-    } else {System.out.println("No hay cantidad suficiente de producto");}
-    return caja;
-  }
-   public double mostrarCaja() {
-    System.out.print("El total de la caja es ");
-    caja=Math.round(caja*100);
-    return caja/100;
-  } 
+    public static void solicitarDatosParaRegistrarCine(){
+        ControlProductos.registrar(new Cine(ControlProductos.solicitarDatosParaRegistrarProductos(),ControlProductos.solicitarDatosDirector()));
+        System.out.println("Registrado exitosamente");
+    }
+
+    public static String solicitarDatosCantante(){
+        System.out.println("Ingrese el cantante: ");
+        String cantante = sc.nextLine();
+        return cantante;
+    }
+
+    public static void registrar(Musica musica) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO_MUSICA, true));
+            bufferedWriter.write(musica.getNombre() + SEPARADOR_CAMPO + musica.getPrecioUnit() + SEPARADOR_CAMPO
+                + musica.getCantStock() + SEPARADOR_CAMPO + musica.isDisponible() + SEPARADOR_CAMPO+ musica.getCantante() + SEPARADOR_REGISTRO );
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error escribiendo en archivo: " + e.getMessage());
+        }
+    }
+
+    public static void solicitarDatosParaRegistrarMusica(){      
+        ControlProductos.registrar(new Musica(ControlProductos.solicitarDatosParaRegistrarProductos(),ControlProductos.solicitarDatosCantante()));
+        System.out.println("Registrado exitosamente");
+    }
+
+    public static String solicitarDatosVideoconsola(){
+        System.out.println("Ingrese la videoconsola: ");
+        String videoconsola = sc.nextLine();
+        return videoconsola;
+    }
+
+    public static void registrar(Videojuego videoconsola) {
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO_MUSICA, true));
+            bufferedWriter.write(videoconsola.getNombre() + SEPARADOR_CAMPO + videoconsola.getPrecioUnit() + SEPARADOR_CAMPO
+                + videoconsola.getCantStock() + SEPARADOR_CAMPO + videoconsola.isDisponible() + SEPARADOR_CAMPO+ videoconsola.getVideoconsola() + SEPARADOR_REGISTRO );
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error escribiendo en archivo: " + e.getMessage());
+        }
+    }
+
+    public static void solicitarDatosParaRegistrarVideojuegos(){
+        ControlProductos.registrar(new Videojuego(ControlProductos.solicitarDatosParaRegistrarProductos(),ControlProductos.solicitarDatosVideoconsola()));
+        System.out.println("Registrado exitosamente");
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    public void mostrarProductos(Producto[] productos) { 
+        for (int i = 0; i < productos.length; i++) {
+            System.out.print(productos[i]+"n-------n");
+        }
+    }
+
+    public void mostrarNombreProductos(Producto[] productos) { 
+        for (int i = 0; i < productos.length; i++) {
+            System.out.println(i+1 +" "+productos[i].getNombre()+"n");
+        }
+        System.out.println("n------------n");
+    } 
+
+    public double comprarProducto(Producto[] productos, int num, int cantidadUnidades) {
+        if (productos[num-1].isDisponible()) {
+            if (productos[num-1].getCantStock() >= cantidadUnidades){
+                System.out.println("La compra se ha realizado con éxito!!n");
+                productos[num -1].setCantStock(productos[num -1].getCantStock()-cantidadUnidades);
+                return caja+=cantidadUnidades*productos[num-1].getPrecioUnit();
+            } else {System.out.println("No hay cantidad suficiente de producto");}
+        } else {System.out.println("No hay cantidad suficiente de producto");}
+        return caja;
+    }
+
+    public double mostrarCaja() {
+        System.out.print("El total de la caja es ");
+        caja=Math.round(caja*100);
+        return caja/100;
+    } 
 }
