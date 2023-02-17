9d8936fe1bdcfd106a08bada9b61e8a094b4023e
monicamrdam <monicamrdam@gmail.com>
2023-02-17 10:14:25 +0100

    update getter-setter-toString

--- Producto.java
+++ Producto.java
@@ -7,5 +7,28 @@
  */
 public class Producto
 {
+    private String descripcion;
+    private double precio;
     
+    public Producto(String descripcion, double precio){
+        this.descripcion= descripcion;
+        this.precio=precio;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+    public double getPrecio(){
+        return precio;
+    }
+    
+    public void setDescripcion(String descripcion){
+        this.descripcion=descripcion;
+    }
+    public void setPrecio(double precio){
+        this.precio=precio;
+    }
+    @Override
+    public String toString(){
+        return "Producto{" + "descripcion=" + descripcion + ", precio=" + precio+"}";
+    }
 }
