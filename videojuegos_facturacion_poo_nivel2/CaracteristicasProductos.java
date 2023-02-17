bf543903895b38a7e100f277249212b907201850
monicamrdam <monicamrdam@gmail.com>
2023-02-17 18:44:15 +0100

    update toString

--- videojuegos_facturacion_poo_nivel2/CaracteristicasProductos.java
+++ videojuegos_facturacion_poo_nivel2/CaracteristicasProductos.java
@@ -7,46 +7,49 @@
  */
 public class CaracteristicasProductos
 {
-  private String nombre;
-  private double precioUnit;
-  private int cantStock;
-  private boolean disponible = false;
-  
-   public CaracteristicasProductos(String nombre, double precioUnit, int cantStock, boolean disponible) {
-    this.nombre   = nombre;
-    this.precioUnit = precioUnit;
-    this.cantStock  = cantStock;
-    this.disponible = disponible;
+    private String nombre;
+    private double precioUnit;
+    private int cantStock;
+    private boolean disponible = false;
+
+    public CaracteristicasProductos(String nombre, double precioUnit, int cantStock, boolean disponible) {
+        this.nombre   = nombre;
+        this.precioUnit = precioUnit;
+        this.cantStock  = cantStock;
+        this.disponible = disponible;
     }
-  public String getNombre() {
-    return this.nombre;
-  }
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
-  public double getPrecioUnit() {
-    return this.precioUnit;
-  }
-  public void setPrecioUnit(double precioUnit) {
-    this.precioUnit = precioUnit;
-  }
-  public int getCantStock() {
-    return this.cantStock;
-  }
-  public void setCantStock(int cantStock) {
-    this.cantStock = cantStock;
-  }
-  public boolean isDisponible() {
-    if(getCantStock()>0) this.disponible = true;
-    return this.disponible;
-  }
-  public void setDisponible(boolean disponible) {
-    this.disponible = disponible;
-  }
-  @Override
-  public String toString() {
-    return  "Nombre: "      +this.getNombre()+    "n"+
-        "Precio unidad: " +this.getPrecioUnit()+  " €n"+
-        "En Stock: "    +this.getCantStock()+ "n";
-  }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioUnit() {
+        return this.precioUnit;
+    }
+
+    public void setPrecioUnit(double precioUnit) {
+        this.precioUnit = precioUnit;
+    }
+
+    public int getCantStock() {
+        return this.cantStock;
+    }
+
+    public void setCantStock(int cantStock) {
+        this.cantStock = cantStock;
+    }
+
+    public boolean isDisponible() {
+        if(getCantStock()>0) this.disponible = true;
+        return this.disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
 }
