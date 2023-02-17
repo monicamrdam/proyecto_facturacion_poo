43fcd42d4da67f5b55d3d8213aee94ebf68e2026
monicamrdam <monicamrdam@gmail.com>
2023-02-17 18:43:00 +0100

    update toString

--- videojuegos_facturacion_poo_nivel2/Producto.java
+++ videojuegos_facturacion_poo_nivel2/Producto.java
@@ -11,38 +11,51 @@ public abstract class Producto
     private double precioUnit;
     private int cantStock;
     private boolean disponible;
-     private CaracteristicasProductos cproductos;
-     public Producto(CaracteristicasProductos cproductos){
-         this.nombre=cproductos.getNombre();
-         this.precioUnit=cproductos.getPrecioUnit();
-         this.cantStock=cproductos.getCantStock();
-         this.disponible=cproductos.isDisponible();
-     }
-     public String getNombre() {
-    return nombre;
-  }
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
-  public double getPrecioUnit() {
-    return this.precioUnit;
-  }
-  public void setPrecioUnit(double precioUnit) {
-    this.precioUnit = precioUnit;
-  }
-  public int getCantStock() {
-    return this.cantStock;
-  }
-  public void setCantStock(int cantStock) {
-    this.cantStock = cantStock;
-  }
-  public boolean isDisponible() {
-    if(getCantStock()>0) this.disponible = true;
-    return this.disponible;
-  }
-  public void setDisponible(boolean disponible) {
-    this.disponible = disponible;
-  }
-  
-  
+    private CaracteristicasProductos cproductos;
+    public Producto(CaracteristicasProductos cproductos){
+        this.nombre=cproductos.getNombre();
+        this.precioUnit=cproductos.getPrecioUnit();
+        this.cantStock=cproductos.getCantStock();
+        this.disponible=cproductos.isDisponible();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioUnit() {
+        return this.precioUnit;
+    }
+
+    public void setPrecioUnit(double precioUnit) {
+        this.precioUnit = precioUnit;
+    }
+
+    public int getCantStock() {
+        return this.cantStock;
+    }
+
+    public void setCantStock(int cantStock) {
+        this.cantStock = cantStock;
+    }
+
+    public boolean isDisponible() {
+        if(getCantStock()>0) this.disponible = true;
+        return this.disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return  "Nombre: "      +this.getNombre()+    "\n"+
+        "Precio unidad: " +this.getPrecioUnit()+  " €\n"+
+        "En Stock: "    +this.getCantStock()+ "\n";
+    }
 }
